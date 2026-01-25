c301f398f8de8fe5e566193b81b8fddd6e986b50
AMM <ansmartin@users.noreply.github.com>
2026-01-25 22:42:59 +0100

    box pasa a ser lista no fija

--- scripts/box.py
+++ scripts/box.py
@@ -1,40 +1,30 @@
 class PokemonBox:
 
     def __init__(self):
-        self.size = 20
-        self.max_position = self.size-1
+        self.max_size = 20
         self.init_box()
     
 
     def init_box(self):
-        self.box = [ None for n in range(self.size) ]
-        self.number_of_pokemon_stored = 0
+        self.box = []
 
     def is_full(self):
-        return self.number_of_pokemon_stored==self.size
+        return len(self.box)==self.max_size
     
     def position_is_in_range(self, position):
-        return position>=0 and position<self.size
-
+        return position>=0 and position<len(self.box)
 
     def save_pokemon(self, pokemon_id):
         if(self.is_full()):
             return False
 
-        for n in range(self.size):
-            if(self.box[n] is None):
-                self.box[n] = pokemon_id
-                self.number_of_pokemon_stored+=1
-                return True
-
-        return False
+        self.box.append(pokemon_id)
+        return True
 
     def delete_pokemon(self, position):
         if(self.position_is_in_range(position)):
-            if(self.box[position]):
-                self.box[position] = None
-                self.number_of_pokemon_stored-=1
-                return True
+            self.box.pop(position)
+            return True
 
         return False
     
@@ -44,8 +34,5 @@ class PokemonBox:
         else:
             return None
 
-    def get_box(self):
-        if(self.number_of_pokemon_stored==0):
-            return None
-        else:
-            return self.box
+    def get_length(self):
+        return len(self.box)
